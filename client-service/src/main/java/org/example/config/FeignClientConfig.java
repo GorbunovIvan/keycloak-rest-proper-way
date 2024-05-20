da5068c068d6890e7746f13cbfd9c77fb0a296a5
GorbunovIvan <110302058+GorbunovIvan@users.noreply.github.com>
2024-05-20 09:33:53 +0300

    “client-service” - now it interacts with remote "protected-service" taking into account that it is secured with OAuth2.0 (Keycloak).

--- client-service/src/main/java/org/example/config/FeignClientConfig.java
+++ client-service/src/main/java/org/example/config/FeignClientConfig.java
@@ -1,9 +1,32 @@
 package org.example.config;
 
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 
 @Configuration
 @EnableFeignClients(basePackages = "org.example.service")
 public class FeignClientConfig {
+
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
+    private String clientId;
+
+    @Bean
+    public RequestInterceptor oauth2FeignRequestInterceptor(OAuth2AuthorizedClientManager clientManager) {
+        return requestTemplate -> {
+            var authorizedClient = clientManager.authorize(
+                    OAuth2AuthorizeRequest.withClientRegistrationId("keycloak")
+                            .principal(clientId)
+                            .build()
+            );
+            if (authorizedClient != null) {
+                var accessToken = authorizedClient.getAccessToken().getTokenValue();
+                requestTemplate.header("Authorization", "Bearer " + accessToken);
+            }
+        };
+    }
 }
